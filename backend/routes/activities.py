cd69cb02a1fb02ada9e5afa486c972a55200ec5f
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-04-23 15:44:09 +0100

    chore: first API

---
+++ backend/routes/activities.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, status
+
+from services.home_activities import HomeActivities
+
+
+router = APIRouter(tags=["activities"])
+
+@router.get("/homeactivities",status_code=status.HTTP_200_OK)
+def get_home_activities():
+    data = HomeActivities().run()
+    return data
