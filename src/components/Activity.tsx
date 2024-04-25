4d774169eda4ee7990a98606b6afa618d34da502
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-04-25 15:49:16 +0100

    fix: fixed css

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -6,7 +6,9 @@ const Activity = () => {
     <Box
       sx={{
         borderBottom: "solid grey 1px",
-        height: "120px",
+        minHeight: "120px",
+        maxHeight: "300px",
+        position: "relative",
       }}
     >
       <Box
@@ -19,17 +21,26 @@ const Activity = () => {
         }}
       >
         <Avatar src="/static/images/avatar/1.jpg" />
-        <Box sx={{ height: "40%" }}>
+        <Box
+          sx={{
+            minHeight: "40%",
+            minWidth: "70%",
+            maxWidth: "80%",
+            backgroundColor: "gold",
+            wordWrap: "break-word",
+          }}
+        >
           <Typography variant="body1">Akmal Popalzi</Typography>
           <Typography variant="body2">@akmal98</Typography>
-          <Typography variant="body1">Cloud is fun!!</Typography>
+          <Typography variant="body1">Cloud is fun!</Typography>
         </Box>
         <Box
           sx={{
             display: "flex",
-            alignItems: "end",
             flexDirection: "column",
-            width: "70%",
+            position: "absolute",
+            top: 0,
+            right: 20,
           }}
         >
           <Typography variant="body1">0m ago</Typography>
