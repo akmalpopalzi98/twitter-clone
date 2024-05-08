74246479ab3735402ef55598b78242c949271a79
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-05-08 12:23:14 +0100

    chore: styling sign up page

--- src/pages/signUpPage.tsx
+++ src/pages/signUpPage.tsx
@@ -1,11 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { FormEvent } from "react";
 
-const signUpPage = () => {
+const SignUpPage = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
-    <Box sx={{ height: "100vh", backgroundColor: "rgb( 74, 35, 90 )" }}>
+    <Box sx={{ height: "100vh", backgroundColor: "rgb(74, 35, 90)" }}>
       <Typography
         sx={{
-          backgroundColor: "rgb( 23, 32, 42  )",
+          backgroundColor: "rgb(23, 32, 42)",
           textAlign: "center",
           padding: "20px",
         }}
@@ -15,17 +21,45 @@ const signUpPage = () => {
       </Typography>
       <Box
         sx={{
-          backgroundColor: "rgb( 23, 32, 42  )",
-          height: "50vh",
-          margin: "20px",
+          backgroundColor: "rgb(23, 32, 42)",
+          height: "60vh",
           borderRadius: "20px",
+          padding: "1px",
+          marginTop: "50px",
+          display: "flex",
+          flexDirection: "column",
         }}
+        component="form"
+        onSubmit={handleSubmit}
       >
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Typography sx={{ textAlign: "center" }}>Name</Typography>
+          <input style={{ width: "20%", marginTop: "10px" }} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Typography sx={{ textAlign: "center" }}>Username</Typography>
+          <input style={{ width: "20%", marginTop: "10px" }} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
           }}
         >
           <Typography sx={{ textAlign: "center" }}>
@@ -33,9 +67,41 @@ const signUpPage = () => {
           </Typography>
           <input style={{ width: "20%", marginTop: "10px" }} />
         </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Typography sx={{ textAlign: "center" }}>New password</Typography>
+          <input style={{ width: "20%", marginTop: "10px" }} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Typography sx={{ textAlign: "center" }}>Confirm Password</Typography>
+          <input style={{ width: "20%", marginTop: "10px" }} />
+        </Box>
+        <Button
+          sx={{
+            backgroundColor: "rgb(74, 35, 90)",
+            color: "white",
+            margin: "30px auto",
+          }}
+          type="submit"
+        >
+          Submit
+        </Button>
       </Box>
     </Box>
   );
 };
 
-export default signUpPage;
+export default SignUpPage;
