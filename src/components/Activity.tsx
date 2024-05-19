8da9ed003cdd4874628e05d11fc34eded034bb2b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-05-19 15:33:19 +0100

    fix: formatting time for each activity

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -4,6 +4,49 @@ import ActivityButtons from "./ActivityButtons";
 import { ActivityType } from "../types";
 
 const Activity = ({ activityItem }: { activityItem: ActivityType }) => {
+  const created_date = new Date(activityItem.created_at);
+  const expire_date = new Date(activityItem.expires_at);
+  const current_date = new Date();
+
+  // Calculate time differences
+  const createdAttimeDiffUnix = current_date.getTime() - created_date.getTime();
+  const createdAtTimeDiffMinutes = createdAttimeDiffUnix / (1000 * 60);
+  const createdAtTimeDiffHours = createdAtTimeDiffMinutes / 60;
+  const createdAtTimeDiffDays = createdAtTimeDiffHours / 24;
+
+  let formatedCreatedAtDate;
+  if (createdAtTimeDiffDays >= 1) {
+    formatedCreatedAtDate = `${Math.round(createdAtTimeDiffDays)} days ago`;
+  } else if (createdAtTimeDiffHours >= 1) {
+    formatedCreatedAtDate = `${Math.round(createdAtTimeDiffHours)} hours ago`;
+  } else if (createdAtTimeDiffMinutes >= 1) {
+    formatedCreatedAtDate = `${Math.round(createdAtTimeDiffMinutes)} mins ago`;
+  } else {
+    formatedCreatedAtDate = `just now`;
+  }
+
+  const expiresAtTimeDiffUnix = expire_date.getTime() - current_date.getTime();
+  const expiresAtTimeDiffMinutes = expiresAtTimeDiffUnix / (1000 * 60);
+  const expiresAtTimeDiffHours = expiresAtTimeDiffMinutes / 60;
+  const expiresAtTimeDiffDays = expiresAtTimeDiffHours / 24;
+
+  let formattedExpiresAtDate;
+  if (expiresAtTimeDiffDays >= 1) {
+    formattedExpiresAtDate = `Expires in ${Math.round(
+      expiresAtTimeDiffDays
+    )} days`;
+  } else if (expiresAtTimeDiffHours >= 1) {
+    formattedExpiresAtDate = `Expires in ${Math.round(
+      expiresAtTimeDiffHours
+    )} hours`;
+  } else if (expiresAtTimeDiffMinutes >= 1) {
+    formattedExpiresAtDate = `Expires in ${Math.round(
+      expiresAtTimeDiffMinutes
+    )} mins`;
+  } else {
+    formattedExpiresAtDate = `Expires soon`;
+  }
+
   return (
     <Box
       sx={{
@@ -45,8 +88,8 @@ const Activity = ({ activityItem }: { activityItem: ActivityType }) => {
             right: 20,
           }}
         >
-          <Typography variant="body1">{activityItem.created_at}</Typography>
-          <Typography variant="body2">{activityItem.expires_at}</Typography>
+          <Typography variant="body1">{formatedCreatedAtDate}</Typography>
+          <Typography variant="body2">{formattedExpiresAtDate}</Typography>
         </Box>
       </Box>
       <Box sx={{ height: "30%", marginTop: "10px" }}>
