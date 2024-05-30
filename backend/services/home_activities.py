a62aef7c9647a8e44c943b584915e90bac80e80b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-05-30 11:21:59 +0100

    fix: fixed bug in sql query

--- backend/services/home_activities.py
+++ backend/services/home_activities.py
@@ -13,7 +13,10 @@ class HomeActivities:
             conn = psycopg2.connect(f"postgresql://postgres:{os.getenv('DB_PASSWORD')}@localhost/twitterclone")
             with conn:
                 with conn.cursor() as curr:
-                    curr.execute("SELECT * FROM activities JOIN users ON users.cognito_id = activities.user_id;") 
+                    curr.execute("""SELECT A.id, A.user_id, A.message, A.created_at, A.expires_at, U.name, U.handle
+                        FROM activities AS A
+                        JOIN users AS U ON U.cognito_id = A.user_id
+                        ORDER BY A.created_at DESC;""") 
                     results = curr.fetchall()
                     col_names = [name[0] for name in curr.description]
                     for record in results:
